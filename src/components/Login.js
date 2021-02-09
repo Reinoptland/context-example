6a22d1e882c0e4cb7259f0bdafaead90f381f2fe
Reinoptland <reinoptland@gmail.com>
2021-02-09 10:50:33 +0100

    Form, event listeners / handlers

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
-  return <div>LOGIN</div>;
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  function handleInput(event) {
+    console.log("INPUT:", event.target.name, event.target.value);
+
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); // no refresh
+    console.log("credentials:", credentials);
+  }
+
+  console.log(credentials);
+
+  return (
+    <div>
+      LOGIN
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            onChange={handleInput}
+            value={credentials.email}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            onChange={handleInput}
+            value={credentials.password}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 }
