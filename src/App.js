92c860799f68e8cbabf779983a7f892b2feba107
Reinoptland <reinoptland@gmail.com>
2021-02-09 11:55:21 +0100

    Make custom header with children

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Login from "./components/Login";
 import TodoList from "./components/TodoList";
 import AuthContext from "./contexts/AuthContext";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   const [userId, setUserId] = useState(null); // move it to a dedicated component
@@ -11,6 +12,10 @@ function App() {
   return (
     <AuthContext.Provider value={{ userId: userId, setUserId: setUserId }}>
       <div className="App">
+        <MyHeader text="CHECK OUT CONTEXT, MY STUDENTS" />
+        <MyHeader>
+          CHECK OUT <span style={{ color: "red" }}>CONTEXT</span> MY STUDENTS
+        </MyHeader>
         <header className="App-header">
           <Login />
           <TodoList />
---
+++ src/components/MyHeader.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function MyHeader(props) {
+  console.log(props);
+  return <h2>{props.children}</h2>;
+}
